a7c0b8590dc62a5c3232351249b2393af1d0dd03
Simon MacDonald <simon.macdonald@gmail.com>
2019-11-28 10:33:23 -0500

    🐛 DEVEP-1705: Stock repo has different kirby format

--- src/ManifestUtils/index.js
+++ src/ManifestUtils/index.js
@@ -1,10 +1,27 @@
-function stripManifestPath(path, urlPrefix = '') {
+function stripManifestPath(path, { org = '', name = '', branch = '' } = {}) {
   if (!path) {
-    return ''
+    return '';
   }
+  let urlPrefix = '';
+  if (org) {
+    urlPrefix += org
+  }
+  if (name) {
+    urlPrefix += urlPrefix !== '' ? '/' + name : name
+  }
+  if (branch) {
+    urlPrefix += urlPrefix !== '' ? '/' + branch : branch
+  }
+  // Normal case with org/name/branch
   let location = path.toLowerCase().indexOf(urlPrefix.toLowerCase())
   if (location > -1) {
     return path.substring(location + urlPrefix.length)
+  }
+  // Exception case with only name in url
+  else if (path.toLowerCase().indexOf(name.toLowerCase() > -1)) {
+    return path.substring(
+      path.toLowerCase().indexOf(name.toLowerCase()) + name.length
+    )
   } else {
     return path
   }
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,8 @@
-import Header from './Header'
-import Footer from './Footer'
-import Feedback from './Feedback'
-import Nav from './Nav'
-import { stripManifestPath } from './ManifestUtils'
+import Header from './Header';
+import Footer from './Footer';
+import Feedback from './Feedback';
+import Nav from './Nav';
+import { stripManifestPath } from './ManifestUtils';
 
 describe('Header', () => {
   it('is truthy', () => {
@@ -33,48 +33,84 @@ describe('stripManifestPath', () => {
     expect(stripManifestPath).toBeTruthy()
   })
   it('empty to return empty', () => {
-    expect(stripManifestPath('', 'fake/path')).toEqual('')
+    expect(stripManifestPath('', { org: 'fake', name: 'path' })).toEqual('')
   })
   it('lower case to work', () => {
     expect(
-      stripManifestPath(
-        'adobedocs/adobeio-events/master/using.md',
-        'adobedocs/adobeio-events/master'
-      )
+      stripManifestPath('adobedocs/adobeio-events/master/using.md', {
+        org: 'adobedocs',
+        name: 'adobeio-events',
+        branch: 'master'
+      })
     ).toEqual('/using.md')
   })
   it('upper case to work', () => {
     expect(
-      stripManifestPath(
-        'AdobeDocs/adobeio-events/master/using.md',
-        'AdobeDocs/adobeio-events/master'
-      )
+      stripManifestPath('AdobeDocs/adobeio-events/master/using.md', {
+        org: 'AdobeDocs',
+        name: 'adobeio-events',
+        branch: 'master'
+      })
     ).toEqual('/using.md')
   })
   it('mixed case to work', () => {
     expect(
-      stripManifestPath(
-        'AdobeDocs/adobeio-events/master/using.md',
-        'adobedocs/adobeio-events/master'
-      )
+      stripManifestPath('AdobeDocs/adobeio-events/master/using.md', {
+        org: 'adobedocs',
+        name: 'adobeio-events',
+        branch: 'master'
+      })
     ).toEqual('/using.md')
   })
   it('reversed mixed case to work', () => {
     expect(
-      stripManifestPath(
-        'adobedocs/adobeio-events/master/using.md',
-        'AdobeDocs/adobeio-events/master'
-      )
+      stripManifestPath('adobedocs/adobeio-events/master/using.md', {
+        org: 'AdobeDocs',
+        name: 'adobeio-events',
+        branch: 'master'
+      })
     ).toEqual('/using.md')
   })
   it('no url prefix to leave path unchanged', () => {
     expect(
-      stripManifestPath('adobedocs/adobeio-events/master/using.md', '')
+      stripManifestPath('adobedocs/adobeio-events/master/using.md', {})
     ).toEqual('adobedocs/adobeio-events/master/using.md')
   })
   it('undefined url prefix to leave path unchanged', () => {
     expect(
       stripManifestPath('adobedocs/adobeio-events/master/using.md')
     ).toEqual('adobedocs/adobeio-events/master/using.md')
+  })
+  it('only repo name is in manifest path all options', () => {
+    expect(
+      stripManifestPath('stock-api-docs/docs/01-getting-started.md', {
+        org: 'adobe',
+        name: 'stock-api-docs',
+        branch: 'master'
+      })
+    ).toEqual('/docs/01-getting-started.md')
+  })
+  it('only repo name is in manifest path missing org', () => {
+    expect(
+      stripManifestPath('stock-api-docs/docs/01-getting-started.md', {
+        name: 'stock-api-docs',
+        branch: 'master'
+      })
+    ).toEqual('/docs/01-getting-started.md')
+  })
+  it('only repo name is in manifest path missing branch', () => {
+    expect(
+      stripManifestPath('stock-api-docs/docs/01-getting-started.md', {
+        org: 'adobe',
+        name: 'stock-api-docs'
+      })
+    ).toEqual('/docs/01-getting-started.md')
+  })
+  it('only repo name is in manifest path missing org and branch', () => {
+    expect(
+      stripManifestPath('stock-api-docs/docs/01-getting-started.md', {
+        name: 'stock-api-docs'
+      })
+    ).toEqual('/docs/01-getting-started.md')
   })
 })
