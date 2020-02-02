b0d2796443c292e91a095784f8e987f298c7d4b2
Simon MacDonald <simon.macdonald@gmail.com>
2020-02-02 11:27:44 -0800

    ♿  Nav landmark not unique

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -1,70 +1,68 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { navigate } from 'gatsby';
-import { stripManifestPath, defaultFocus } from '../ManifestUtils';
+import React from "react";
+import PropTypes from "prop-types";
+import { navigate } from "gatsby";
+import { stripManifestPath, defaultFocus } from "../ManifestUtils";
 
-const { SideNav, SideNavItem } = require('@react/react-spectrum/SideNav')
+const { SideNav, SideNavItem } = require("@react/react-spectrum/SideNav");
 
 const nav = (data, gitInfo) => {
   return data.map((node, index) => {
-    const updatedPath = stripManifestPath(node.path, gitInfo)
-    console.log(updatedPath)
+    const updatedPath = stripManifestPath(node.path, gitInfo);
+    console.log(updatedPath);
     return (
       <SideNavItem
         key={index}
-        aria-current='page'
+        aria-current="page"
         isNested={false}
         disabled={false}
         defaultExpanded={true}
         onClick={() => {
           if (updatedPath) {
             if (
-              updatedPath.startsWith('http://') ||
-              updatedPath.startsWith('https://')
+              updatedPath.startsWith("http://") ||
+              updatedPath.startsWith("https://")
             ) {
-              document.location.href = updatedPath
+              document.location.href = updatedPath;
             } else {
-              navigate(updatedPath)
+              navigate(updatedPath);
             }
           }
         }}
         label={node.title}
-        target='_self'
+        target="_self"
         value={node.title}
       >
-        {node.pages ? nav(node.pages, gitInfo) : ''}
+        {node.pages ? nav(node.pages, gitInfo) : ""}
       </SideNavItem>
-    )
-  })
+    );
+  });
 };
 
 const Nav = ({ data, selected, gitInfo }) => {
   return (
-    <nav>
-      <SideNav
-        autoFocus={true}
-        defaultValue={defaultFocus(data, selected, gitInfo)}
-        isNested={false}
-        manageTabIndex={false}
-        typeToSelect
-        variant='multiLevel'
-      >
-        {nav(data, gitInfo)}
-      </SideNav>
-    </nav>
-  )
+    <SideNav
+      autoFocus={true}
+      defaultValue={defaultFocus(data, selected, gitInfo)}
+      isNested={false}
+      manageTabIndex={false}
+      typeToSelect
+      variant="multiLevel"
+    >
+      {nav(data, gitInfo)}
+    </SideNav>
+  );
 };
 
 Nav.propTypes = {
   data: PropTypes.array,
   selected: PropTypes.string,
   gitInfo: PropTypes.object
-}
+};
 
 Nav.defaultProps = {
   data: [],
-  selected: '',
+  selected: "",
   gitInfo: {}
-}
+};
 
-export default Nav
+export default Nav;
