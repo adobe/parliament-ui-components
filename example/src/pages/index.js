1ffba0192e9189e544be6592925ebd1d1f95bf0c
Simon MacDonald <simon.macdonald@gmail.com>
2020-03-24 19:12:43 -0400

    👕  Fix liniting errors

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,139 +1,139 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Provider from '@react/react-spectrum/Provider'
-import { Header, Footer, Feedback, Nav } from 'parliament-ui-components'
+import React from "react"
+import PropTypes from "prop-types"
+import Provider from "@react/react-spectrum/Provider"
+import { Header, Footer, Feedback, Nav } from "parliament-ui-components"
 
 const data = {
-  name: 'Events',
-  version: '1.0.0',
-  description: 'Events',
-  author: 'Emily Walpole',
-  view_type: 'mdbook',
-  meta_keywords: 'adobe i/o,events',
-  meta_description: 'default description',
-  publish_date: '22/03/2018',
+  name: "Events",
+  version: "1.0.0",
+  description: "Events",
+  author: "Emily Walpole",
+  view_type: "mdbook",
+  meta_keywords: "adobe i/o,events",
+  meta_description: "default description",
+  publish_date: "22/03/2018",
   show_edit_github_banner: false,
-  base_path: 'https://raw.githubusercontent.com',
+  base_path: "https://raw.githubusercontent.com",
   pages: [
     {
-      importedFileName: 'readme',
+      importedFileName: "readme",
       pages: [
         {
-          importedFileName: 'webhook-docs-intro',
+          importedFileName: "webhook-docs-intro",
           pages: [],
-          path: 'adobedocs/adobeio-events/master/intro/webhook_docs_intro.md',
-          title: 'Introduction to Webhooks',
+          path: "adobedocs/adobeio-events/master/intro/webhook_docs_intro.md",
+          title: "Introduction to Webhooks"
         },
         {
-          importedFileName: 'events-api',
+          importedFileName: "events-api",
           pages: [],
-          path: 'adobedocs/adobeio-events/master/intro/events-api.md',
-          title: 'Working with Events Using Management API',
+          path: "adobedocs/adobeio-events/master/intro/events-api.md",
+          title: "Working with Events Using Management API"
         },
         {
-          importedFileName: 'journaling_api',
+          importedFileName: "journaling_api",
           pages: [],
-          path: 'adobedocs/adobeio-events/master/intro/journaling_api.md',
-          title: 'Journaling API',
+          path: "adobedocs/adobeio-events/master/intro/journaling_api.md",
+          title: "Journaling API"
         },
         {
-          importedFileName: 'test.md',
+          importedFileName: "test.md",
           pages: [],
-          path: 'test/path/test.md',
-          title: 'Test of Focus',
+          path: "test/path/test.md",
+          title: "Test of Focus"
         },
         {
-          importedFileName: 'authentication.md',
+          importedFileName: "authentication.md",
           pages: [],
-          path: 'https://adobe.io/authentication',
-          title: 'Test of Remote Link',
-        },
+          path: "https://adobe.io/authentication",
+          title: "Test of Remote Link"
+        }
       ],
-      path: 'adobedocs/adobeio-events/master/readme.md',
-      title: 'Getting Started',
+      path: "adobedocs/adobeio-events/master/readme.md",
+      title: "Getting Started"
     },
     {
-      importedFileName: 'using',
+      importedFileName: "using",
       pages: [
         {
-          importedFileName: 'aem-event-setup',
+          importedFileName: "aem-event-setup",
           pages: [],
-          path: 'adobedocs/adobeio-events/master/using/aem-event-setup.md',
-          title: 'Setting Up AEM Events',
+          path: "adobedocs/adobeio-events/master/using/aem-event-setup.md",
+          title: "Setting Up AEM Events"
         },
         {
-          importedFileName: 'analytics-triggers-event-setup',
+          importedFileName: "analytics-triggers-event-setup",
           pages: [],
           path:
-            'adobedocs/adobeio-events/master/using/analytics-triggers-event-setup.md',
-          title: 'Integrate Analytics Triggers',
+            "adobedocs/adobeio-events/master/using/analytics-triggers-event-setup.md",
+          title: "Integrate Analytics Triggers"
         },
         {
-          importedFileName: 'cc-asset-event-setup',
+          importedFileName: "cc-asset-event-setup",
           pages: [],
-          path: 'adobedocs/adobeio-events/master/using/cc-asset-event-setup.md',
-          title: 'Setting Up Creative Cloud Asset Events',
+          path: "adobedocs/adobeio-events/master/using/cc-asset-event-setup.md",
+          title: "Setting Up Creative Cloud Asset Events"
         },
         {
-          importedFileName: 'create-event-integration',
-          pages: [],
-          path:
-            'https://www.adobe.io/apis/experiencecloud/cloud-manager/docs.html#!AdobeDocs/cloudmanager-api-docs/master/create-event-integration.md',
-          title: 'Cloud Manager',
-        },
-        {
-          importedFileName: 'gdpr-io-events',
+          importedFileName: "create-event-integration",
           pages: [],
           path:
-            'https://www.adobe.io/apis/experienceplatform/gdpr/docs/alldocs.html#!api-specification/markdown/narrative/gdpr/gdpr-io-events.md',
-          title: 'GDPR Events',
+            "https://www.adobe.io/apis/experiencecloud/cloud-manager/docs.html#!AdobeDocs/cloudmanager-api-docs/master/create-event-integration.md",
+          title: "Cloud Manager"
         },
+        {
+          importedFileName: "gdpr-io-events",
+          pages: [],
+          path:
+            "https://www.adobe.io/apis/experienceplatform/gdpr/docs/alldocs.html#!api-specification/markdown/narrative/gdpr/gdpr-io-events.md",
+          title: "GDPR Events"
+        }
       ],
-      path: 'adobedocs/adobeio-events/master/using.md',
-      title: 'Using Adobe I/O Events',
+      path: "adobedocs/adobeio-events/master/using.md",
+      title: "Using Adobe I/O Events"
     },
     {
-      importedFileName: 'support',
+      importedFileName: "support",
       pages: [
         {
-          importedFileName: 'debug',
+          importedFileName: "debug",
           pages: [],
-          path: 'adobedocs/adobeio-events/master/support/debug.md',
-          title: 'Debugging',
+          path: "adobedocs/adobeio-events/master/support/debug.md",
+          title: "Debugging"
         },
         {
-          importedFileName: 'tracing',
+          importedFileName: "tracing",
           pages: [],
-          path: 'adobedocs/adobeio-events/master/support/tracing.md',
-          title: 'Tracing',
+          path: "adobedocs/adobeio-events/master/support/tracing.md",
+          title: "Tracing"
         },
         {
-          importedFileName: 'Forums',
+          importedFileName: "Forums",
           pages: [],
-          path: 'https://forums.adobe.com/community/adobe-io/adobe-io-events',
-          title: 'Forums',
+          path: "https://forums.adobe.com/community/adobe-io/adobe-io-events",
+          title: "Forums"
         },
         {
-          importedFileName: 'faq',
+          importedFileName: "faq",
           pages: [],
-          path: 'adobedocs/adobeio-events/master/support/faq.md',
-          title: 'Frequently Asked Questions (FAQ)',
+          path: "adobedocs/adobeio-events/master/support/faq.md",
+          title: "Frequently Asked Questions (FAQ)"
         },
         {
-          importedFileName: 'release_notes',
+          importedFileName: "release_notes",
           pages: [],
-          path: 'adobedocs/adobeio-events/master/support/release_notes.md',
-          title: 'Release Notes',
-        },
+          path: "adobedocs/adobeio-events/master/support/release_notes.md",
+          title: "Release Notes"
+        }
       ],
-      path: 'adobedocs/adobeio-events/master/support.md',
-      title: 'Support',
-    },
-  ],
+      path: "adobedocs/adobeio-events/master/support.md",
+      title: "Support"
+    }
+  ]
 }
 
 const index = ({ location }) => {
-  const title = 'Test Title'
+  const title = "Test Title"
   return (
     <Provider theme="lightest">
       <Header siteTitle={title} />
@@ -141,7 +141,7 @@ const index = ({ location }) => {
       <Nav
         data={data.pages}
         selected="test/path/test.md"
-        gitInfo={{ org: 'adobedocs', name: 'adobeio-events', branch: 'master' }}
+        gitInfo={{ org: "adobedocs", name: "adobeio-events", branch: "master" }}
       />
       <Feedback
         gitUrl="https://git.corp.adobe.com/devrel/parliament-client-template"
@@ -154,11 +154,11 @@ const index = ({ location }) => {
 }
 
 index.propTypes = {
-  location: PropTypes.object,
+  location: PropTypes.object
 }
 
 index.defaultProps = {
-  location: {},
+  location: {}
 }
 
 export default index
