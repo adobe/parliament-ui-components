9e62a397a0c10cc4d8b5dc52c21fd29217e24895
Simon MacDonald <simon.macdonald@gmail.com>
2020-03-27 15:38:43 -0400

    🐛 DEVEP-1880: Footer component should dynamical display the correct year

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,88 +1,91 @@
 import React from "react"
 
-const Footer = () => (
-  <div
-    style={{
-      flexBasis: "100%",
-      maxWidth: "100%",
-      padding: "0px 8px"
-    }}
-  >
-    <footer style={{ display: "flex", flexDirection: "column" }}>
-      <div style={{ width: "100%", margin: "0 auto", fontSize: "11px" }}>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            flexWrap: "wrap",
-            float: "right",
-            padding: "8px !important"
-          }}
-        >
-          <ul style={{ listStyle: "none" }}>
-            <li
-              style={{
-                display: "inline"
-              }}
-            >
-              Copyright © 2019 Adobe. All rights reserved. /{" "}
-            </li>
-            <li
-              style={{
-                display: "inline"
-              }}
-            >
-              <a
+const Footer = () => {
+  const year = new Date().getFullYear()
+  return (
+    <div
+      style={{
+        flexBasis: "100%",
+        maxWidth: "100%",
+        padding: "0px 8px"
+      }}
+    >
+      <footer style={{ display: "flex", flexDirection: "column" }}>
+        <div style={{ width: "100%", margin: "0 auto", fontSize: "11px" }}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              flexWrap: "wrap",
+              float: "right",
+              padding: "8px !important"
+            }}
+          >
+            <ul style={{ listStyle: "none" }}>
+              <li
                 style={{
-                  textDecoration: "none",
-                  color: "inherit",
-                  transition: "color .1s ease,fill .1s ease"
+                  display: "inline"
                 }}
-                href="https://www.adobe.com/privacy.html"
               >
-                Privacy
-              </a>{" "}
-              /{" "}
-            </li>
-            <li
-              style={{
-                display: "inline"
-              }}
-            >
-              <a
+                Copyright © {year} Adobe. All rights reserved. /{" "}
+              </li>
+              <li
                 style={{
-                  textDecoration: "none",
-                  color: "inherit",
-                  transition: "color .1s ease,fill .1s ease"
+                  display: "inline"
                 }}
-                href="https://www.adobe.com/legal/terms.html"
               >
-                Terms of Use
-              </a>{" "}
-              /{" "}
-            </li>
-            <li
-              style={{
-                display: "inline"
-              }}
-            >
-              <a
+                <a
+                  style={{
+                    textDecoration: "none",
+                    color: "inherit",
+                    transition: "color .1s ease,fill .1s ease"
+                  }}
+                  href="https://www.adobe.com/privacy.html"
+                >
+                  Privacy
+                </a>{" "}
+                /{" "}
+              </li>
+              <li
                 style={{
-                  textDecoration: "none",
-                  color: "inherit",
-                  transition: "color .1s ease,fill .1s ease"
+                  display: "inline"
                 }}
-                href="https://www.adobe.com/privacy/cookies.html"
               >
-                Cookies
-              </a>
-            </li>
-          </ul>
+                <a
+                  style={{
+                    textDecoration: "none",
+                    color: "inherit",
+                    transition: "color .1s ease,fill .1s ease"
+                  }}
+                  href="https://www.adobe.com/legal/terms.html"
+                >
+                  Terms of Use
+                </a>{" "}
+                /{" "}
+              </li>
+              <li
+                style={{
+                  display: "inline"
+                }}
+              >
+                <a
+                  style={{
+                    textDecoration: "none",
+                    color: "inherit",
+                    transition: "color .1s ease,fill .1s ease"
+                  }}
+                  href="https://www.adobe.com/privacy/cookies.html"
+                >
+                  Cookies
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </footer>
-  </div>
-)
+      </footer>
+    </div>
+  )
+}
 
 export default Footer
