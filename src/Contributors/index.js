fe071a5dbab3d24814e1021a9256131cb0b65ece
Simon MacDonald <smacdona@adobe.com>
2020-06-22 17:45:20 -0400

    🎉 Created Contributors component (#22)
    * 🖼️  Add Contributors component stub
    
    * ✅  Add basic Contributors test
    
    * ✅  Stub for Contributors stories
    
    * 🎉  Contributors component take one

---
+++ src/Contributors/index.js
@@ -0,0 +1,81 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+
+const Avatars = styled.div`
+  display: inline-flex;
+  flex-direction: row-reverse;
+  padding-left: 32px;
+`
+
+const Avatar = styled.span`
+  margin-left: -16px;
+  position: relative;
+  border: 3px solid white;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  bacground: white;
+`
+
+const AvatarImg = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+`
+
+const Contributors = ({ gitUrl, committers, date }) => {
+  const lifoComitters = committers.slice(0, 5).reverse()
+  return (
+    <a
+      css={css`
+        text-decoration: none;
+        color: inherit;
+      `}
+      href={gitUrl}
+      target='_blank'
+      rel='noreferrer'
+    >
+      <div
+        css={css`
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+        `}
+      >
+        <Avatars>
+          {lifoComitters &&
+            lifoComitters.map((committer, index) => {
+              return (
+                <Avatar key={index}>
+                  <AvatarImg src={committer} />
+                </Avatar>
+              )
+            })}
+        </Avatars>
+        <span
+          css={css`
+            padding-left: 16px;
+          `}
+        >
+          Last updated {date}
+        </span>
+      </div>
+    </a>
+  )
+}
+
+Contributors.propTypes = {
+  committers: PropTypes.array,
+  date: PropTypes.string,
+  gitUrl: PropTypes.string
+}
+
+Contributors.defaultProps = {
+  committers: [],
+  date: '',
+  gitUrl: ''
+}
+
+export default Contributors
