6252a514195803d2613a3ea9b8aa1b03ac3efd44
Simon MacDonald <simon.macdonald@gmail.com>
2020-07-07 09:49:10 -0400

    🔧  Prepare for open sourcing

--- src/TableOfContents/__stories__/mockData.js
+++ src/TableOfContents/__stories__/mockData.js
@@ -1,3 +1,14 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
 const mockData = {
   regular:
     '<ul>\n<li>\n<p><a href="/CLA-Management.md/#cla-management">CLA Management</a></p>\n<ul>\n<li><a href="/CLA-Management.md/#outbound-clas">Outbound CLAs</a></li>\n<li><a href="/CLA-Management.md/#inbound-clas">Inbound CLAs</a></li>\n</ul>\n</li>\n</ul>',
