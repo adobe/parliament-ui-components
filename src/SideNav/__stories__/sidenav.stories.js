6f32290f7f3b01254ff0ac88e0d252297e5fb46f
Simon MacDonald <simon.macdonald@gmail.com>
2020-07-14 23:01:05 -0400

    🧱  SideNav component

---
+++ src/SideNav/__stories__/sidenav.stories.js
@@ -0,0 +1,84 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+import React from 'react'
+
+import { Provider } from '@react-spectrum/provider'
+import { theme } from '@react-spectrum/theme-default'
+
+import Star from '@spectrum-icons/workflow/Star'
+
+import SideNav from '../index'
+
+const icon = <Star size='S' />
+
+const Wrapper = (storyFn) => {
+  return (
+    <Provider theme={theme} colorScheme='light'>
+      {storyFn()}
+    </Provider>
+  )
+}
+
+export default {
+  title: 'components/SideNav',
+  decorators: [Wrapper]
+}
+
+export const FlatItems = () => {
+  const props = {
+    items: [
+      { name: 'Aardvark' },
+      { name: 'Kangaroo' },
+      { name: 'Snake' },
+      { name: 'Hippo' }
+    ],
+    selectedKeys: ['Kangaroo'],
+    disabledKeys: ['Snake'],
+    onSelectionChange: (item) => console.log(item)
+  }
+  return <SideNav {...props} />
+}
+
+export const FlatItemsWithIcon = () => {
+  const props = {
+    items: [
+      { name: 'Aardvark', icon: icon },
+      { name: 'Kangaroo', icon: icon },
+      { name: 'Snake', icon: icon },
+      { name: 'Hippo', icon: icon }
+    ],
+    selectedKeys: ['Kangaroo'],
+    disabledKeys: ['Snake'],
+    onSelectionChange: (item) => console.log(item)
+  }
+  return <SideNav {...props} />
+}
+
+export const Multilevel = () => {
+  const props = {
+    items: [
+      {
+        name: 'Mammals',
+        children: [
+          { name: 'Aardvark' },
+          { name: 'Kangaroo' },
+          { name: 'Hippo' }
+        ]
+      },
+      { name: 'Reptiles', children: [{ name: 'Snake' }] }
+    ],
+    selectedKeys: ['Kangaroo'],
+    disabledKeys: ['Snake'],
+    onSelectionChange: (item) => console.log(item)
+  }
+  return <SideNav {...props} />
+}
