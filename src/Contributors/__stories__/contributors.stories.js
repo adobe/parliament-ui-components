e5ff2ab4ec151ef51ead477493c12b9f825585bb
Stephan Ringel <ringel@adobe.com>
2020-07-17 10:37:02 +0200

    Added Contributors, Feedback and Hero

--- src/Contributors/__stories__/contributors.stories.js
+++ src/Contributors/__stories__/contributors.stories.js
@@ -14,7 +14,7 @@ import React from 'react'
 import { Provider } from '@react-spectrum/provider'
 import { theme } from '@react-spectrum/theme-default'
 
-import Contributors from '../index'
+import { Contributors } from '../index'
 
 export default {
   title: 'components/Contributors'
@@ -22,11 +22,9 @@ export default {
 
 export const oneCommitter = () => {
   const props = {
-    committers: [
-      'https://avatars3.githubusercontent.com/u/353180?s=460&u=0b019002d891c7a1eaa469c18574f4a7a1b6e4a5&v=4'
-    ],
+    contributors: ['https://github.com/icaraps'],
     date: 'May 20, 2020',
-    gitUrl: ''
+    href: ''
   }
 
   return (
@@ -38,13 +36,13 @@ export const oneCommitter = () => {
 
 export const threeCommitter = () => {
   const props = {
-    committers: [
-      'https://avatars3.githubusercontent.com/u/353180?s=460&u=0b019002d891c7a1eaa469c18574f4a7a1b6e4a5&v=4',
-      'https://avatars3.githubusercontent.com/u/125516?s=460&v=4',
-      'https://avatars1.githubusercontent.com/u/25260?s=460&v=4'
+    contributors: [
+      'https://github.com/icaraps',
+      'https://github.com/macdonst',
+      'https://github.com/jcalcaben'
     ],
     date: 'May 20, 2020',
-    gitUrl: ''
+    href: ''
   }
 
   return (
@@ -56,15 +54,15 @@ export const threeCommitter = () => {
 
 export const fiveCommitter = () => {
   const props = {
-    committers: [
-      'https://avatars3.githubusercontent.com/u/353180?s=460&u=0b019002d891c7a1eaa469c18574f4a7a1b6e4a5&v=4',
-      'https://avatars3.githubusercontent.com/u/125516?s=460&v=4',
-      'https://avatars1.githubusercontent.com/u/25260?s=460&v=4',
-      'https://avatars3.githubusercontent.com/u/37266?s=460&v=4',
-      'https://avatars3.githubusercontent.com/u/164498?s=460&v=4'
+    contributors: [
+      'https://github.com/icaraps',
+      'https://github.com/macdonst',
+      'https://github.com/jcalcaben',
+      'https://github.com/shazron',
+      'https://github.com/simonwex'
     ],
     date: 'May 20, 2020',
-    gitUrl: ''
+    href: ''
   }
 
   return (
@@ -76,16 +74,16 @@ export const fiveCommitter = () => {
 
 export const dontShowSixCommitter = () => {
   const props = {
-    committers: [
-      'https://avatars3.githubusercontent.com/u/353180?s=460&u=0b019002d891c7a1eaa469c18574f4a7a1b6e4a5&v=4',
-      'https://avatars3.githubusercontent.com/u/125516?s=460&v=4',
-      'https://avatars1.githubusercontent.com/u/25260?s=460&v=4',
-      'https://avatars3.githubusercontent.com/u/37266?s=460&v=4',
-      'https://avatars3.githubusercontent.com/u/164498?s=460&v=4',
-      'https://avatars1.githubusercontent.com/u/65071?s=460&u=98da53a69859351bf94928a7236681414e84f5f9&v=4'
+    contributors: [
+      'https://github.com/icaraps',
+      'https://github.com/macdonst',
+      'https://github.com/jcalcaben',
+      'https://github.com/shazron',
+      'https://github.com/simonwex',
+      'https://github.com/hirenoble'
     ],
     date: 'May 20, 2020',
-    gitUrl: ''
+    href: ''
   }
 
   return (
--- src/Contributors/index.js
+++ src/Contributors/index.js
@@ -1,92 +1,86 @@
 /*
-Copyright 2020 Adobe. All rights reserved.
-This file is licensed to you under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License. You may obtain a copy
-of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-OF ANY KIND, either express or implied. See the License for the specific language
-governing permissions and limitations under the License.
-*/
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
 import PropTypes from 'prop-types'
-import styled from '@emotion/styled'
+import { Flex } from '@react-spectrum/layout'
 
-const Avatars = styled.div`
-  display: inline-flex;
-  flex-direction: row-reverse;
-  padding-left: 32px;
-`
-
-const Avatar = styled.span`
-  margin-left: -16px;
-  position: relative;
-  border: 3px solid white;
-  width: 32px;
-  height: 32px;
-  border-radius: 50%;
-  bacground: white;
-`
-
-const AvatarImg = styled.img`
-  width: 32px;
-  height: 32px;
-  border-radius: 50%;
-`
-
-const Contributors = ({ gitUrl, committers, date }) => {
-  const lifoComitters = committers.slice(0, 5).reverse()
-  return (
-    <a
-      css={css`
-        text-decoration: none;
-        color: inherit;
-      `}
-      href={gitUrl}
-      target='_blank'
-      rel='noopener noreferrer'
-    >
+const Contributors = ({ href = '#', contributors = [], date }) => (
+  <a
+    href={href}
+    target='_blank'
+    rel='noopener noreferrer nofollow'
+    css={css`
+      text-decoration: none;
+      color: inherit;
+    `}
+  >
+    <Flex alignItems='center'>
       <div
         css={css`
-          display: flex;
-          flex-direction: row;
-          align-items: center;
+          display: inline-flex;
+          padding-left: var(--spectrum-global-dimension-static-size-200);
         `}
       >
-        <Avatars>
-          {lifoComitters &&
-            lifoComitters.map((committer, index) => {
-              return (
-                <Avatar key={index}>
-                  <AvatarImg src={committer} />
-                </Avatar>
-              )
-            })}
-        </Avatars>
-        <span
-          css={css`
-            padding-left: 16px;
-          `}
-        >
-          Last updated {date}
-        </span>
+        {contributors.slice(0, 5).map((contributor, index) => (
+          <span
+            key={index}
+            css={css`
+              margin-left: calc(
+                -1 * var(--spectrum-global-dimension-static-size-200)
+              );
+              position: relative;
+              border: var(--spectrum-global-dimension-static-size-40) solid
+                white;
+              width: var(--spectrum-global-dimension-static-size-400);
+              height: var(--spectrum-global-dimension-static-size-400);
+              border-radius: var(--spectrum-global-dimension-static-percent-50);
+              background: var(--spectrum-global-color-gray-50);
+            `}
+          >
+            <img
+              alt={contributor}
+              src={
+                contributor.startsWith('https://github.com/')
+                  ? `${contributor}.png`
+                  : contributor
+              }
+              css={css`
+                width: var(--spectrum-global-dimension-static-size-400);
+                height: var(--spectrum-global-dimension-static-size-400);
+                border-radius: var(
+                  --spectrum-global-dimension-static-percent-50
+                );
+              `}
+            />
+          </span>
+        ))}
       </div>
-    </a>
-  )
-}
+      <span
+        css={css`
+          padding-left: var(--spectrum-global-dimension-static-size-200);
+        `}
+      >
+        {date && `Last updated ${date}`}
+      </span>
+    </Flex>
+  </a>
+)
 
 Contributors.propTypes = {
-  committers: PropTypes.array,
-  date: PropTypes.string,
-  gitUrl: PropTypes.string
+  href: PropTypes.string,
+  contributors: PropTypes.array,
+  date: PropTypes.string
 }
 
-Contributors.defaultProps = {
-  committers: [],
-  date: '',
-  gitUrl: ''
-}
-
-export default Contributors
+export { Contributors }
