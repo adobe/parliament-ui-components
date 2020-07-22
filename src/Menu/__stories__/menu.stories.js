e0ff17f090322821c6f9137e3ff73b4c970d7494
Simon MacDonald <simon.macdonald@gmail.com>
2020-07-22 17:13:50 -0400

    🧱 Menu, Item and Popover

---
+++ src/Menu/__stories__/menu.stories.js
@@ -0,0 +1,62 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/Itemcenses/ItemCENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+import React from 'react'
+
+import { Provider } from '@react-spectrum/provider'
+import { theme } from '@react-spectrum/theme-default'
+
+import { Menu, Item } from '../index'
+
+const Wrapper = (storyFn) => {
+  return (
+    <Provider theme={theme} colorScheme='light'>
+      {storyFn()}
+    </Provider>
+  )
+}
+
+export default {
+  title: 'components/Menu',
+  decorators: [Wrapper]
+}
+
+export const MenuChildren = () => {
+  const props = {}
+
+  return (
+    <Menu {...props}>
+      <Item isSelected>Ballard</Item>
+      <Item>Fremont</Item>
+      <Item>Greenwood</Item>
+      <Item isDivider />
+      <Item isDisabled>United States of America</Item>
+    </Menu>
+  )
+}
+
+export const MenuItems = () => {
+  const props = {
+    onAction: (e) => {
+      console.log(e)
+    },
+    items: [
+      { name: 'Cut' },
+      { name: 'Copy' },
+      { name: 'Paste' },
+      { name: 'Replace' }
+    ],
+    selectedKeys: ['Copy'],
+    disabledKeys: ['Replace']
+  }
+
+  return <Menu {...props} />
+}
