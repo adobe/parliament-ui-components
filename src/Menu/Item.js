b9b24130c4ae9b5e2b1a2fc758dd300c7ca33845
Simon MacDonald <simon.macdonald@gmail.com>
2020-07-22 17:13:50 -0400

    🧱 Menu, Item and Popover

---
+++ src/Menu/Item.js
@@ -0,0 +1,55 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+
+import '@spectrum-css/menu'
+
+const Item = ({
+  children,
+  key,
+  item,
+  onAction,
+  isSelected = false,
+  isDisabled = false,
+  isDivider = false
+}) =>
+  isDivider ? (
+    <li className='spectrum-Menu-divider' role='separator' key={key} />
+  ) : (
+    <li
+      className={classNames([
+        'spectrum-Menu-item',
+        { 'is-selected': isSelected },
+        { 'is-disabled': isDisabled }
+      ])}
+      role='option'
+      aria-selected={isSelected}
+      aria-disabled={isDisabled}
+      tabIndex='0'
+      key={key}
+      onClick={() => {
+        onAction && onAction(item || children)
+      }}
+    >
+      <span className='spectrum-Menu-itemLabel'>{children}</span>
+    </li>
+  )
+
+Item.propTypes = {
+  isSelected: PropTypes.bool,
+  isDisabled: PropTypes.bool
+}
+
+export default Item
---
+++ src/Popover/__stories__/popover.stories.js
@@ -0,0 +1,85 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+import React from 'react'
+
+import { Provider } from '@react-spectrum/provider'
+import { theme } from '@react-spectrum/theme-default'
+
+import Popover from '../index'
+import { Menu, Item } from '../../Menu'
+import SearchField from '../../SearchField'
+
+export default {
+  title: 'components/Popover'
+}
+
+export const OpenPopover = () => {
+  const props = { isOpen: true }
+  const itemProps = { onAction: (e) => console.log(e) }
+
+  return (
+    <Provider theme={theme} colorScheme='light'>
+      <Popover {...props}>
+        <Menu>
+          <Item {...itemProps} isSelected>
+            Ballard
+          </Item>
+          <Item {...itemProps}>Fremont</Item>
+          <Item {...itemProps}>Greenwood</Item>
+          <Item isDivider />
+          <Item {...itemProps} isDisabled>
+            United States of America
+          </Item>
+        </Menu>
+      </Popover>
+    </Provider>
+  )
+}
+
+export const OpenPopoverItems = () => {
+  const props = {
+    isOpen: true,
+    style: { position: 'absolute', left: '0px', top: '32px', zIndex: '1000' }
+  }
+  const menuProps = {
+    onAction: (e) => {
+      console.log(e)
+    },
+    items: [
+      { name: 'Cut' },
+      { name: 'Copy' },
+      { name: 'Paste' },
+      { name: 'Replace' }
+    ],
+    selectedKeys: ['Copy'],
+    disabledKeys: ['Replace']
+  }
+
+  return (
+    <Provider theme={theme} colorScheme='light'>
+      <div style={{ position: 'relative' }}>
+        <SearchField
+          onSubmit={(searchTerm) => {
+            console.log(searchTerm)
+          }}
+        />
+        <Popover {...props}>
+          <Menu {...menuProps} />
+        </Popover>
+        <p>
+          This is a bunch of text that the popover should float over top of.
+        </p>
+        <p>Moar text!</p>
+      </div>
+    </Provider>
+  )
+}
