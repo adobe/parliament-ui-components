6cd9c26854609d2e3af1611c2102e3576b7e7ab1
Simon MacDonald <simon.macdonald@gmail.com>
2020-08-07 11:51:51 -0400

    📝  Add license headers

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,24 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 module.exports = ({ config }) => {
   // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
   // ========================================================
-  config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
+  config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
 
   // Add Babel rules
   // ========================================================
   // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
-  config.module.rules[0].use[0].loader = require.resolve('babel-loader');
+  config.module.rules[0].use[0].loader = require.resolve('babel-loader')
 
   // use @babel/preset-react for JSX and env (instead of staged presets)
   config.module.rules[0].use[0].options.presets = [
@@ -14,19 +26,19 @@ module.exports = ({ config }) => {
     require.resolve('@babel/preset-env'),
     // Emotion preset must run BEFORE reacts preset to properly convert css-prop.
     // Babel preset-ordering runs reversed (from last to first). Emotion has to be after React preset.
-    require.resolve('@emotion/babel-preset-css-prop'),
-  ];
+    require.resolve('@emotion/babel-preset-css-prop')
+  ]
 
   config.module.rules[0].use[0].options.plugins = [
     // use @babel/plugin-proposal-class-properties for class arrow functions
     require.resolve('@babel/plugin-proposal-class-properties'),
     // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
-    require.resolve('babel-plugin-remove-graphql-queries'),
-  ];
+    require.resolve('babel-plugin-remove-graphql-queries')
+  ]
 
   // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
   // ========================================================
-  config.resolve.mainFields = ['browser', 'module', 'main'];
+  config.resolve.mainFields = ['browser', 'module', 'main']
 
-  return config;
-};
+  return config
+}
