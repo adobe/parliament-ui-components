b0fc68c1c3fe304109909075be2c4fd5d7a9c5bf
Simon MacDonald <simon.macdonald@gmail.com>
2020-08-10 14:17:32 -0400

    🖼️  Render info as coverted markdown

--- src/OpenApi/Info.js
+++ src/OpenApi/Info.js
@@ -14,7 +14,7 @@ import { css, jsx } from '@emotion/core'
 import React from 'react'
 import PropTypes from 'prop-types'
 
-// import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from 'react-markdown'
 
 import '@spectrum-css/typography'
 
@@ -38,7 +38,7 @@ const Info = ({ info = {} }) => {
           margin-bottom: 60px;
         `}
       >
-        {info.description}
+        <ReactMarkdown source={info.description} />
       </p>
     </React.Fragment>
   )
