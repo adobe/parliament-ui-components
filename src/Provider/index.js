d2196e47ee35a624f0926feb2fdeff62a859ac9e
Coleton <colsmith@adobe.com>
2021-03-09 11:59:19 -0700

    :octopus: MethodPicker test

--- src/Provider/index.js
+++ src/Provider/index.js
@@ -1,18 +1,17 @@
-import React from 'react'
-
-import '@spectrum-css/vars/dist/spectrum-global.css'
-import '@spectrum-css/vars/dist/spectrum-medium.css'
-import '@spectrum-css/vars/dist/spectrum-large.css'
-import '@spectrum-css/vars/dist/spectrum-light.css'
-import '@spectrum-css/typography'
-import '@adobe/focus-ring-polyfill'
+import { defaultTheme, Provider as RSProvider } from "@adobe/react-spectrum"
 
 export const Provider = ({ children, ...props }) => (
-  <div
-    className='spectrum spectrum--light spectrum--medium'
-    dir='ltr'
-    {...props}
+  <RSProvider
+    theme={defaultTheme}
+    colorScheme="light"
+    scale="medium"
   >
-    {children}
-  </div>
-)
+    <div
+      className='spectrum spectrum--light spectrum--medium'
+      dir='ltr'
+      {...props}
+    >
+      {children}
+    </div>
+  </RSProvider>
+  )
