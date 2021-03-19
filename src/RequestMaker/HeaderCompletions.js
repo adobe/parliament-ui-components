f80a92b178bbd43504446de4fc084cfe40a5bd96
Simon MacDonald <simon.macdonald@gmail.com>
2021-03-19 13:48:15 -0400

    🔨  Tweak defaultMenuItems to be completions

---
+++ src/RequestMaker/HeaderCompletions.js
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2021 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+export const completions = [
+  { name: 'Accept' },
+  { name: 'Accept-Charset' },
+  { name: 'Accept-Encoding' },
+  { name: 'Accept-Language' },
+  { name: 'Access-Control-Request-Headers' },
+  { name: 'Access-Control-Request-Method' },
+  { name: 'Authorization' },
+  { name: 'Cache-Control' },
+  { name: 'Content-MD5' },
+  { name: 'Content-Length' },
+  { name: 'Content-Transfer-Encoding' },
+  { name: 'Content-Type' },
+  { name: 'Cookie' },
+  { name: 'Cookie 2' },
+  { name: 'Date' },
+  { name: 'Expect' },
+  { name: 'From' },
+  { name: 'Host' },
+  { name: 'If-Match' },
+  { name: 'If-Modified-Since' },
+  { name: 'If-None-Match' },
+  { name: 'If-Range' },
+  { name: 'If-Unmodified-Since' },
+  { name: 'Keep-Alive' },
+  { name: 'Max-Forwards' },
+  { name: 'Origin' },
+  { name: 'Pragma' },
+  { name: 'Proxy-Authorization' },
+  { name: 'Range' },
+  { name: 'Referer' },
+  { name: 'TE' },
+  { name: 'Trailer' },
+  { name: 'Transfer-Encoding' },
+  { name: 'Upgrade' },
+  { name: 'User-Agent' },
+  { name: 'Via' },
+  { name: 'Warning' },
+  { name: 'X-Requested-With' },
+  { name: 'X-Do-Not-Track' },
+  { name: 'DNT' },
+  { name: 'x-api-key' },
+  { name: 'Connection' }
+]
--- src/RequestMaker/RequestParameters.js
+++ src/RequestMaker/RequestParameters.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Adobe. All rights reserved.
+ * Copyright 2021 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -17,6 +17,7 @@ import { CodeGen } from './CodeGen'
 import { RequestBody } from './RequestBody'
 import { RequestProvider, useRequest } from './RequestContext'
 import { ParameterTable } from './ParameterTable'
+import { completions } from './HeaderCompletions'
 
 const findChild = (childrenArray, type) => {
   const child = childrenArray.filter(
@@ -46,50 +47,6 @@ const createParametersArray = (childrenArray, type) => {
     ...filterChildren(element)
   ]
 }
-const defaultMenuItems = [
-  { name: 'Accept' },
-  { name: 'Accept-Charset' },
-  { name: 'Accept-Encoding' },
-  { name: 'Accept-Language' },
-  { name: 'Access-Control-Request-Headers' },
-  { name: 'Access-Control-Request-Method' },
-  { name: 'Authorization' },
-  { name: 'Cache-Control' },
-  { name: 'Content-MD5' },
-  { name: 'Content-Length' },
-  { name: 'Content-Transfer-Encoding' },
-  { name: 'Content-Type' },
-  { name: 'Cookie' },
-  { name: 'Cookie 2' },
-  { name: 'Date' },
-  { name: 'Expect' },
-  { name: 'From' },
-  { name: 'Host' },
-  { name: 'If-Match' },
-  { name: 'If-Modified-Since' },
-  { name: 'If-None-Match' },
-  { name: 'If-Range' },
-  { name: 'If-Unmodified-Since' },
-  { name: 'Keep-Alive' },
-  { name: 'Max-Forwards' },
-  { name: 'Origin' },
-  { name: 'Pragma' },
-  { name: 'Proxy-Authorization' },
-  { name: 'Range' },
-  { name: 'Referer' },
-  { name: 'TE' },
-  { name: 'Trailer' },
-  { name: 'Transfer-Encoding' },
-  { name: 'Upgrade' },
-  { name: 'User-Agent' },
-  { name: 'Via' },
-  { name: 'Warning' },
-  { name: 'X-Requested-With' },
-  { name: 'X-Do-Not-Track' },
-  { name: 'DNT' },
-  { name: 'x-api-key' },
-  { name: 'Connection' }
-]
 
 const RequestParameters = ({ url, children }) => {
   const [options, dispatch] = useRequest()
@@ -136,7 +93,7 @@ const RequestParameters = ({ url, children }) => {
           <Content marginTop='size-250' marginStart='size-125'>
             <ParameterTable
               items={options.headers}
-              defaultMenuItems={defaultMenuItems}
+              completions={completions}
               callback={(data) => {
                 dispatch({
                   type: RequestProvider.ACTION_TYPES.SET_HEADERS,
