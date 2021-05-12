025fd6e99af582dd0ba1f7fbdca07406e6f1c659
Simon MacDonald <simon.macdonald@gmail.com>
2021-05-12 18:06:09 -0400

    🎉  Basic table

--- src/JsonSchema/index.js
+++ src/JsonSchema/index.js
@@ -6,12 +6,93 @@
  *
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- * OF ANY KIND, either express or implied. See the License for the specific language
+ * OF ANY KIND, either express or implied. See the License for the schemaific language
  * governing permissions and limitations under the License.
  */
 
 import React from 'react'
+import { Flex, Heading, Text, View } from '@adobe/react-spectrum'
+import { InlineCode } from '../InlineCode'
+import { Table, TBody, Tr, Td } from '../Table'
 
-export const JsonSchema = ({ spec = {}, ...props }) => {
-  return <div {...props}>JsonSchema</div>
+export const JsonSchema = ({ schema = {}, ...props }) => {
+  const { title, type } = schema
+  return (
+    <div {...props}>
+      {title && <Heading level={1}>{title}</Heading>}
+      {type && <Heading level={2}>Type: {type}</Heading>}
+      <JsonSchemaProperties schema={schema} />
+    </div>
+  )
+}
+
+const JsonSchemaProperties = ({ schema }) => {
+  const { properties, required = [] } = schema
+  console.log(Object.keys(properties) > 0)
+  return Object.keys(properties).length > 0 ? (
+    <Table>
+      <TBody>
+        {Object.keys(properties).map((key) => (
+          <Tr key={key}>
+            <Td>
+              <Flex direction='column'>
+                <View>{key}</View>
+                {required.includes(key) && (
+                  <View>
+                    <span
+                      style={{
+                        color: 'var(--spectrum-global-color-red-400)'
+                      }}
+                    >
+                      required
+                    </span>
+                  </View>
+                )}
+              </Flex>
+            </Td>
+            <Td>
+              <JsonSchemaPropertyDetails name={key} schema={schema} />
+            </Td>
+          </Tr>
+        ))}
+      </TBody>
+    </Table>
+  ) : null
+}
+
+const JsonSchemaPropertyDetails = ({ name, schema }) => {
+  const {
+    type,
+    description,
+    default: defaultValue,
+    example,
+    enum: enumValue
+  } = schema.properties[name]
+  return (
+    <Flex direction='column'>
+      {type && (
+        <View>
+          <InlineCode>{type}</InlineCode>
+        </View>
+      )}
+      {defaultValue && (
+        <View>
+          Default: <InlineCode>{defaultValue}</InlineCode>
+        </View>
+      )}
+      {enumValue && (
+        <View>
+          Enum:{' '}
+          {enumValue.map((item) => (
+            <InlineCode key={item}>{item}</InlineCode>
+          ))}
+        </View>
+      )}
+      {description && (
+        <View>
+          <Text>{description}</Text>
+        </View>
+      )}
+    </Flex>
+  )
 }
--- src/JsonSchema/stories/JsonSchema.stories.js
+++ src/JsonSchema/stories/JsonSchema.stories.js
@@ -13,10 +13,41 @@
 import React from 'react'
 import { JsonSchema } from '../index'
 
+const personSchema = {
+  $id: 'https://example.com/person.schema.json',
+  $schema: 'https://json-schema.org/draft/2020-12/schema',
+  title: 'Person',
+  type: 'object',
+  properties: {
+    firstName: {
+      type: 'string',
+      description: "The person's first name."
+    },
+    lastName: {
+      type: 'string',
+      description: "The person's last name."
+    },
+    age: {
+      description: 'Age in years which must be equal to or greater than zero.',
+      type: 'integer',
+      minimum: 0
+    },
+    huntingSkill: {
+      type: 'string',
+      description: 'The measured skill for hunting',
+      default: 'lazy',
+      example: 'adventurous',
+      enum: ['clueless', 'lazy', 'adventurous', 'aggressive']
+    }
+  },
+  required: ['firstName', 'lastName']
+}
+
 export default {
   title: 'components/JsonSchema'
 }
 
 export const Default = () => {
-  return <JsonSchema />
+  const props = { schema: personSchema }
+  return <JsonSchema {...props} />
 }
